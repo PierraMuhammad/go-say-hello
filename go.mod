a30fdea042f83b31b13adb326f0a630dc25a30f7
PierraMuhammad <pierrams1@gmail.com>
2025-02-11 18:43:43 +0700

    update version 2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/PierraMuhammad/go-say-hello
+module github.com/PierraMuhammad/go-say-hello/v2
 
 go 1.23.6
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
-package go_say_hello
+package gosayhello
 
-func SayHello() string {
-	return "Hello"
+func SayHello(name string) string {
+	return "Hello " + name
 }
